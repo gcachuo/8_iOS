f8e1cd6af72db5718e4ca856e6302459475dc119
cachu <gcachu.o@gmail.com>
2016-06-13 18:36:12 -0500

    nueva libreria

---
+++ 8_iOS/PortableLibrary_8iOs/manejoDeImagenes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace PortableLibrary_8iOs
+{
+    class manejoDeImagenes
+    {
+        public string DescargarImagen(string url)
+        {
+            try
+            {
+                var webClient = new htt
+                return "true";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
